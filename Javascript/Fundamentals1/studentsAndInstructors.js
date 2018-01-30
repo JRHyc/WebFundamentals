66363b568604f8c0bd7f3a19b376a53fb881cef6
Jack Hyc <Jacek.Hyc@gmail.com>
2018-01-30 13:06:02 -0600

    added JS scroll assignment

---
+++ Javascript/Fundamentals1/studentsAndInstructors.js
@@ -0,0 +1,24 @@
+//create a program that outputs:
+// Michael Jordan
+// John Rosales
+// Mark Guillen
+// KB Tone1
+
+var students = [ 
+     {first_name :  'Michael', last_name : 'Jordan'},
+     {first_name : 'John', last_name : 'Rosales'},
+     {first_name : 'Mark', last_name : 'Guillen'},
+     {first_name : 'KB', last_name : 'Tonel'}
+]
+
+function firstAndLast(arr){
+    for(var i = 0; i < arr.length; i++){
+        console.log(students[i].first_name, students[i].last_name);
+    }
+    
+}
+firstAndLast(students);
+
+
+
+
